5c0cbe1ee4a74832b2b304aca16ae450acd8e637
JasurSalimov <jasurtsalimov@gmail.com>
2024-08-25 01:12:54 +0500

    Adds two more files to repo

---
+++ Colibri.xcframework/ios-arm64/Colibri.framework/Headers/Colibri.h
@@ -0,0 +1,18 @@
+//
+//  Colibri.h
+//  Colibri
+//
+//  Created by Jasur Salimov on 8/24/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Colibri.
+FOUNDATION_EXPORT double ColibriVersionNumber;
+
+//! Project version string for Colibri.
+FOUNDATION_EXPORT const unsigned char ColibriVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Colibri/PublicHeader.h>
+
+
